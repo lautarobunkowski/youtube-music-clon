acab2691592ad6ec07050463ec498c61a9cd3f50
lautaro <lautibunko@gmail.com>
2024-02-19 19:40:16 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Layout from "@components/Layout.tsx";
+
+function App() {
+
+  return (
+    <div className="min-h-screen bg-[#020202]">
+      <Layout/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Layout.tsx
@@ -0,0 +1,13 @@
+import menu from "@icons/menu.svg";
+
+const Layout = () => {
+  return (
+    <div>
+        <nav>
+            <img src={menu} alt="menu" className="text-white"/>
+        </nav>
+    </div>
+  )
+}
+
+export default Layout
